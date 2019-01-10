3d0fa5ab53ded73f512b31bbb682e1be28c8441f
Grant Adams <adamsgd@uw.edu>
2019-01-09 17:23:05 -0800

    Added refrence case to SDATA

--- 12_2018_Runs/12_2018_SData.R
+++ 12_2018_Runs/12_2018_SData.R
@@ -11,84 +11,74 @@ Core.1946.present <- Core.Catches[Core.Catches$Year > 1945, ]
 Core.Catches.Reorg <- merge(Core.1901.1938, Core.1939.1945, by = "Year", all = TRUE)
 Core.Catches.Reorg <- merge(Core.Catches.Reorg, Core.1946.present, by = "Year", all = TRUE)
 
-sir_reference <- HUMPBACK.SIR(file_name = "Reference/Reference",
-                              n_resamples = 10000,
-                              priors = make_prior_list(r_max = make_prior(runif, 0, 0.118),
-                                                       N_obs = make_prior(runif, 500, 40000)),
-                              catch_multipliers = make_multiplier_list(
-                                make_prior(rbest, 0.139, 0.3),
-                                make_prior(runif, 1.25, 1.42), 
-                                make_prior(rnorm, 1.0185, 0.0028)),
-                              premodern_catch_multipliers = make_multiplier_list(
-                                make_prior(rnorm, 1.71, 0.073)),
-                              target.Yr = 2008,
-                              num.haplotypes = 0,
-                              output.Yrs = c(2012, 2018),
-                              abs.abundance = rbind(Abs.Abundance.2008, Abs.Abundance.2012),
-                              rel.abundance = rbind(Rel.Abundance.Pavanato, Rel.Abundance.Branch),
-                              rel.abundance.key = TRUE, # No indices of abundance
-                              count.data = Count.Data,
-                              count.data.key = FALSE,
-                              growth.rate.obs = c(0.074, 0.033, FALSE), # Do not include growth rate
-                              growth.rate.Yrs = c(1995, 1996, 1997, 1998),
-                              catch.data = Core.Catches.Reorg,
-                              premodern_catch_data = merge(PreModern.Catch.Min, PreModern.Catch.Max, by = "Year", all = T),
-                              control = sir_control(threshold = 10 * 6e-9, progress_bar = TRUE))
+sir_reference <- list()
+Rel.Abundance.Branch$ Index <- 2
+rel_abund <- rbind(Rel.Abundance.Pavanato, Rel.Abundance.Branch)
+for(i in 1:2){
+  sir_reference[[i]] <-  HUMPBACK.SIR(file_name = paste0("Reference/Reference", c("","prior")[i]),
+                                      n_resamples = 10000,
+                                      priors = make_prior_list(r_max = make_prior(runif, 0, 0.118),
+                                                               N_obs = make_prior(runif, 500, 40000)),
+                                      catch_multipliers = make_multiplier_list(
+                                        make_prior(rbest, 0.139, 0.3),
+                                        make_prior(runif, 1.25, 1.42), 
+                                        make_prior(rnorm, 1.0185, 0.0028)),
+                                      premodern_catch_multipliers = make_multiplier_list(
+                                        make_prior(rnorm, 1.71, 0.073)),
+                                      target.Yr = 2008,
+                                      num.haplotypes = 0,
+                                      output.Yrs = c(2012, 2018),
+                                      abs.abundance = rbind(Abs.Abundance.2008, Abs.Abundance.2012),
+                                      rel.abundance = rel_abund,
+                                      rel.abundance.key = TRUE, # Indices of abundance
+                                      count.data = Count.Data,
+                                      count.data.key = FALSE,
+                                      growth.rate.obs = c(0.074, 0.033, FALSE), # Do not include growth rate
+                                      growth.rate.Yrs = c(1995, 1996, 1997, 1998),
+                                      catch.data = Core.Catches.Reorg,
+                                      premodern_catch_data = merge(PreModern.Catch.Min, PreModern.Catch.Max, by = "Year", all = T),
+                                      control = sir_control(threshold = 1e-23, progress_bar = TRUE),
+                                      realized_prior = ifelse(1, "FALSE", "TRUE"))
+}
 resample_summary_reference <- summary_sir(sir_reference$resamples_output, object = "Resample_Summary", file_name = "Reference/Reference")
 trajectory_summary_reference <- summary_sir(sir_reference$resamples_trajectories, object = "Trajectory_Summary", file_name = "Reference/Reference")
 plot_trajectory(sir_reference,  file_name = "Reference/Reference")
-plot_density(list(sir_base, sir_reference),  file_name = "Reference/Reference", multiple_sirs = TRUE, lower = c(NA, 22000, 0, NA, 20000, 0, 0.35, 0.6), upper = c(NA, 40000, 2000, NA, 28000, 0.1, NA, NA))
+plot_density(sir_reference,  file_name = "Reference/Reference", multiple_sirs = TRUE, lower = c(NA, 22000, 0, NA, 20000, 0, 0.35, 0.6), upper = c(NA, 40000, 2000, NA, 28000, 0.1, NA, NA))
 save(sir_reference, file = "Reference/sir_scatch6.RData")
 zerbini_table(sir_reference,  file_name = "Reference/Reference")
-
-sir_reference_prior <- HUMPBACK.SIR(file_name = "Reference/Reference",
-                                    n_resamples = 10000,
-                                    priors = make_prior_list(r_max = make_prior(runif, 0, 0.118),
-                                                             N_obs = make_prior(runif, 500, 40000)),
-                                    catch_multipliers = make_multiplier_list(
-                                      make_prior(rbest, 0.139, 0.3),
-                                      make_prior(runif, 1.25, 1.42), 
-                                      make_prior(rnorm, 1.0185, 0.0028)),
-                                    premodern_catch_multipliers = make_multiplier_list(
-                                      make_prior(rnorm, 1.71, 0.073)),
-                                    target.Yr = 2008,
-                                    num.haplotypes = 0,
-                                    output.Yrs = c(2012, 2018),
-                                    abs.abundance = rbind(Abs.Abundance.2008, Abs.Abundance.2012),
-                                    rel.abundance = rbind(Rel.Abundance.Pavanato, Rel.Abundance.Branch),
-                                    rel.abundance.key = TRUE, # No indices of abundance
-                                    count.data = Count.Data,
-                                    count.data.key = FALSE,
-                                    growth.rate.obs = c(0.074, 0.033, FALSE), # Do not include growth rate
-                                    growth.rate.Yrs = c(1995, 1996, 1997, 1998),
-                                    catch.data = Core.Catches.Reorg,
-                                    premodern_catch_data = merge(PreModern.Catch.Min, PreModern.Catch.Max, by = "Year", all = T),
-                                    control = sir_control(threshold = 10 * 6e-9, progress_bar = TRUE),
-                                    realized_prior = TRUE)
 
 
 ################################################################################
 # SDATA Runs
 ################################################################################
 # SData 1
-sir_sdata_1 <- HUMPBACK.SIR(file_name = "SData 1/SData 1",
-                            n_resamples = 10000,
-                            priors = make_prior_list(r_max = make_prior(runif, 0, 0.118),
-                                                     N_obs = make_prior(runif, 500, 40000)),
-                            catch_multipliers = make_multiplier_list(make_prior(1)),
-                            premodern_catch_multipliers = make_multiplier_list(make_prior(1)),
-                            target.Yr = 2012,
-                            num.haplotypes = 0,
-                            output.Yrs = c(2008, 2018),
-                            abs.abundance = rbind(Abs.Abundance.2008, Abs.Abundance.2012),
-                            rel.abundance = Rel.Abundance.Pavanato,
-                            rel.abundance.key = FALSE, # No indices of abundance
-                            count.data = Count.Data,
-                            count.data.key = FALSE,
-                            growth.rate.obs = c(0.074, 0.033, FALSE), # Do not include growth rate
-                            growth.rate.Yrs = c(1995, 1996, 1997, 1998),
-                            catch.data = Core.Catches2,
-                            control = sir_control(threshold = 10 * 4e-9, progress_bar = TRUE))
+sir_sdata_1 <- list()
+for(i in 1:2){
+  sir_sdata_1[[i]] <-  HUMPBACK.SIR(file_name = paste0("SData 1/SData 1", c("","prior")[i]),
+                                      n_resamples = 10000,
+                                      priors = make_prior_list(r_max = make_prior(runif, 0, 0.118),
+                                                               N_obs = make_prior(runif, 500, 40000)),
+                                      catch_multipliers = make_multiplier_list(
+                                        make_prior(rbest, 0.139, 0.3),
+                                        make_prior(runif, 1.25, 1.42), 
+                                        make_prior(rnorm, 1.0185, 0.0028)),
+                                      premodern_catch_multipliers = make_multiplier_list(
+                                        make_prior(rnorm, 1.71, 0.073)),
+                                      target.Yr = 2012,
+                                      num.haplotypes = 0,
+                                      output.Yrs = c(2008, 2018),
+                                      abs.abundance = rbind(Abs.Abundance.2008, Abs.Abundance.2012),
+                                      rel.abundance = rel_abund,
+                                      rel.abundance.key = TRUE, # Indices of abundance
+                                      count.data = Count.Data,
+                                      count.data.key = FALSE,
+                                      growth.rate.obs = c(0.074, 0.033, FALSE), # Do not include growth rate
+                                      growth.rate.Yrs = c(1995, 1996, 1997, 1998),
+                                      catch.data = Core.Catches.Reorg,
+                                      premodern_catch_data = merge(PreModern.Catch.Min, PreModern.Catch.Max, by = "Year", all = T),
+                                      control = sir_control(threshold = 10 * 1e-24, progress_bar = TRUE),
+                                      realized_prior = i-1)
+}
 resample_summary_sdata_1 <- summary_sir(sir_sdata_1$resamples_output, object = "Resample_Summary", file_name = "SData 1/SData 1")
 trajectory_summary_sdata_1 <- summary_sir(sir_sdata_1$resamples_trajectories, object = "Trajectory_Summary", file_name = "SData 1/SData 1")
 plot_trajectory(sir_sdata_1,  file_name = "SData 1/SData 1")
